43f66611aff4dda1fa3c428c3853b873ca956033
Nikhilesh Patil <nikrockstar78@gmail.com>
2026-08-16 16:31:07 +0530

    Add portal settings display

--- main.cpp
+++ main.cpp
@@ -32,6 +32,11 @@ void displayMenu() {
     cout << "4. Logout" << endl;
 }
 
+void displaySettings() {
+    cout << "\n===== Portal Settings =====" << endl;
+    cout << "Theme: Default" << endl;
+    cout << "Notifications: Enabled" << endl;
+}
 
 int main() {
     printWelcomeMessage();
@@ -40,6 +45,7 @@ int main() {
     Student student{"Nikhilesh Patil", 101, "Computer Science"};
     displayStudent(student);
     displayMenu();
+    displaySettings();
 
     cout << "Program finished successfully." << std::endl;
 
